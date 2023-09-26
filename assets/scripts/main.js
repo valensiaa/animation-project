462fc38a9b6bc320bc4efefdf1a5c0df4413a6fd
valensiaa28 <Valentyna@stradigi.eu>
2023-09-26 18:05:03 +0300

    add basic

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,1 +1,21 @@
-import "bootstrap";
+import "./utils/headerTitle";
+import "./utils/scrollTo";
+import Swiper from 'swiper';
+import { Pagination, Navigation } from 'swiper/modules';
+
+const swiper = new Swiper('.js-slider', {
+    modules: [Pagination, Navigation],
+    direction: 'vertical',
+    slidesPerView: 1,
+    spaceBetween: 30,
+    mousewheel: true,
+    parallax: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+})
